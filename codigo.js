9e0c53b655998277bca9e47d90edc443962157f0
jhon1554 <129795045+jhon1554@users.noreply.github.com>
2023-10-25 20:42:41 -0300

    carrusel a medias

--- codigo.js
+++ codigo.js
@@ -1,1 +1,34 @@
-let
+var imagenes = ["../imagenes peliculas/lore.png", "../imagenes peliculas/antman.jpeg", "../imagenes peliculas/scream6.jpg", "../imagenes peliculas/the-mill.png"];
+var contador = 0;
+
+function carrusel(contenedor){
+    contenedor.addEventListener("click", e => {
+        let atras = contenedor.querySelector(".anterior"),
+        adelante = contenedor.querySelector(".siguiente"),
+        img = contenedor.querySelector("img"),
+        tgt = e.target;
+
+        if(tgt === atras){
+            if(contador > 0){
+                img.src = imagenes[contador - 1];
+                contador --;
+            }else{
+                img.src = imagenes[imagenes.length - 1];
+                contador = imagenes.length - 1;
+            }
+        }else if(tgt = adelante){
+            if(contador < imagenes.length - 1){
+                img.src = imagenes[contador + 1];
+                contador ++;
+            }else{
+                img.src = imagenes[0];
+                contador = 0;
+            }
+        }
+    })
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    let contenedor = document.querySelector(".conteiner");
+    carrusel(contenedor)
+})
