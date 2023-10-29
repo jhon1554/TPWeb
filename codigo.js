c937820da8b95c21adcec59c47e05937959d539d
jhon1554 <129795045+jhon1554@users.noreply.github.com>
2023-10-28 22:38:10 -0300

    carrusel casi terminado

--- codigo.js
+++ codigo.js
@@ -1,27 +1,56 @@
-var imagenes = ["../imagenes peliculas/lore.png", "../imagenes peliculas/antman.jpeg", "../imagenes peliculas/scream6.jpg", "../imagenes peliculas/the-mill.png"];
-var contador = 0;
+let peliculas = [
+    {
+        imagen:"../imagenes peliculas/lore.png",
+        pagina:"../paginas/peli8.html",
+        titulo:"La maldicion del bosque"
+    },
+    {
+        imagen:"../imagenes peliculas/antman.jpeg",
+        pagina:"../paginas/peli3.html",
+        titulo:"Ant man"
+    },{
+        imagen:"../imagenes peliculas/scream6.jpg",
+        pagina:"../paginas/peli2.html",
+        titulo:"Scream 6"
+    },{
+        imagen:"../imagenes peliculas/the-mill.png",
+        pagina:"../paginas/peli6.html",
+        titulo:"The mill"
+    }
+]
+
+let contador = 0;
 
 function carrusel(contenedor){
     contenedor.addEventListener("click", e => {
         let atras = contenedor.querySelector(".anterior"),
         adelante = contenedor.querySelector(".siguiente"),
-        img = contenedor.querySelector("img"),
+        img = contenedor.querySelector(".img-carrusel"),
+        link = contenedor.querySelector(".link-carrusel"),
         tgt = e.target;
 
         if(tgt === atras){
             if(contador > 0){
-                img.src = imagenes[contador - 1];
+                img.src = peliculas[contador - 1].imagen;
+                img.title = peliculas[contador - 1].titulo;
+                link.href = peliculas[contador - 1].pagina;
                 contador --;
             }else{
-                img.src = imagenes[imagenes.length - 1];
-                contador = imagenes.length - 1;
+                img.src = peliculas[peliculas.length - 1].imagen;
+                img.title = peliculas[peliculas.length - 1].titulo;
+                link.href = peliculas[peliculas.length - 1].pagina
+                contador = peliculas.length - 1;
             }
-        }else if(tgt = adelante){
-            if(contador < imagenes.length - 1){
-                img.src = imagenes[contador + 1];
+        }else if(tgt === adelante){
+            if(contador < peliculas.length - 1){
+                img.src = peliculas[contador + 1].imagen;
+                img.title = peliculas[contador + 1].titulo;
+                link.href = peliculas[contador + 1].pagina;
                 contador ++;
             }else{
-                img.src = imagenes[0];
+                img.src = peliculas[0].imagen;
+                img.title = peliculas[0].titulo;
+                link.href = peliculas[0].pagina;
                 contador = 0;
             }
         }
@@ -30,5 +59,5 @@ function carrusel(contenedor){
 
 document.addEventListener("DOMContentLoaded", () => {
     let contenedor = document.querySelector(".conteiner");
-    carrusel(contenedor)
+    carrusel(contenedor);
 })
