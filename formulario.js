db59a4e97efe182b0488bd767df153b6f67e506a
jhon1554 <129795045+jhon1554@users.noreply.github.com>
2023-10-29 23:35:03 -0300

    formulario con errores
    el formulario con unos errores buenos bonitos y baratos

---
+++ formulario.js
@@ -0,0 +1,28 @@
+const nombre = document.getElementById("name"),
+email = document.getElementById("email"),
+pass = document.getElementById("password"),
+form = document.getElementById("form"),
+parrafo = document.getElementById("warnings");
+
+form.addEventListener("submit", e =>{
+    e.preventDefault();
+    let warnings = "";
+    let aux = false;
+    let expreg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    if(nombre.value.lenght < 6){
+        console.log("hola")
+        warnings += "El nombre no es valido <br>";
+        aux = true;
+    }
+    if(!expreg.test(email.value)){
+        warnings += "El correo no es valido <br>";
+        aux = true;
+    }
+    if(pass.value.lenght < 6){
+        warnings += "La contraseña no es valida <br>";
+        aux = true;
+    }
+    if(aux){
+        parrafo.innerHTML = warnings;
+    }
+})
